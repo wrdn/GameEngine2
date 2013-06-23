034a049db6f7856ceb061091b38d7be5bf3355ee
William Dann <williamdann@hotmail.com>
2013-06-23 19:30:56 +0100

    GameEngine2 - Created loadScript function, trace channel macros and extended errors that appear if you load an invalid lua file.

--- experiments/lua_experiments/luajit_experiment/data/first.lua
+++ experiments/lua_experiments/luajit_experiment/data/first.lua
@@ -1,1 +1,12 @@
-print("hello world from lua!")
+function countingForLoop(a,b)
+  for i=a,b do
+    print(i)
+  end
+end
+
+function testPrintFunc(a)
+  print("testPrintFunc: " .. a)
+end
+
+countingForLoop(1,10)
+testPrintFunc("hello")
--- experiments/lua_experiments/luajit_experiment/src/main.cpp
+++ experiments/lua_experiments/luajit_experiment/src/main.cpp
@@ -7,10 +7,12 @@ extern "C"
   #include "lualib.h"
 }
 
-int main()
+#define TRACE_MSG(channel, msg, ...) { printf("[%s]", channel); printf(" "); printf(msg, __VA_ARGS__); printf("\n"); }
+#define ERROR_TRACE(msg, ...) { TRACE_MSG("ERROR", msg, __VA_ARGS__); }
+
+// Open various common libraries
+void lua_open_libs(lua_State *l)
 {
-  lua_State *l = lua_open();
-  
   luaopen_base(l);
   luaopen_math(l);
   luaopen_string(l);
@@ -21,10 +23,44 @@ int main()
   luaopen_bit(l);
   luaopen_jit(l);
   luaopen_ffi(l);
+}
+
+// Loads a script and prints errors messages if any
+bool loadScript(lua_State *l, const char* luaFile)
+{
+  int loadFileRes = luaL_loadfile(l, luaFile);
+  if(loadFileRes != 0)
+  {
+    switch(loadFileRes)
+    {
+      case LUA_ERRSYNTAX:
+	ERROR_TRACE("Syntax error in file %s", luaFile);
+	break;
+      case LUA_ERRMEM:
+	ERROR_TRACE("Memory allocation error loading %s", luaFile);
+	break;
+      case LUA_ERRFILE:
+	ERROR_TRACE("Cannot open/read file %s", luaFile);
+	break;
+      default:
+	ERROR_TRACE("Unknown error occured when loading lua file %s", luaFile);
+	break;
+    }
+    ERROR_TRACE("Lua loading errors: %s", lua_tostring(l, -1)); 
+  }
+  
+  return loadFileRes == 0;
+}
+
+int main()
+{
+  lua_State *l = lua_open();
+  lua_open_libs(l);
   
   char filename[128] = "data/first.lua";
+  loadScript(l, filename);
   
-  luaL_dofile(l, filename);
+  lua_pcall(l, 0,0,0);
   
   lua_close(l);
   
